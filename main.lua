f43b9c2b92acf750100442191a53dcccc91accc4
Lando Icaza <landoicco@gmail.com>
2023-08-18 14:24:19 -0600

    LOVE2D functions added - Load and Update functions added! - Key Pressed/released functions added - Player properties made global

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 -- Set window properties
 local windowWidth, windowHeight = 800, 600
 local player = love.graphics.newImage("survivor.png")
+local playerAngle = 0
 
 love.window.setTitle("Puto el k lea esto")
 love.window.setMode(windowWidth, windowHeight, {
@@ -10,15 +11,35 @@ love.window.setMode(windowWidth, windowHeight, {
 
 -- Define spawn position for the player
 function setPlayerSpawnPosition()
-    local height = player:getHeight()
-    local width = player:getWidth()
+    playerHeight = player:getHeight()
+    playerWidth = player:getWidth()
 
-    initialXPos = (windowWidth / 2) - (width / 2)
-    initialYPos = (windowHeight / 2) - (height / 2)
+    playerXPos = (windowWidth / 2) - (playerWidth / 2)
+    playerYPos = (windowHeight / 2) - (playerHeight / 2)
 end
 
-setPlayerSpawnPosition()
+-- Detect keyboard inputs every frame
+function love.keypressed(key)
+    if key == "e" then
+        text = "E key has been pressed!"
+    end
+end
 
+function love.keyreleased(key)
+    if key == "e" then
+        text = "E key has been released!"
+    end
+end
+
+-- This funtion is called one at the beginning of the game
+function love.load()
+    setPlayerSpawnPosition()
+end
+
+function love.update(dt)
+end
+
+-- Callback function used to draw on screen every frame
 function love.draw()
-    love.graphics.draw(player, initialXPos, initialYPos)
+    love.graphics.draw(player, playerXPos, playerYPos)
 end
