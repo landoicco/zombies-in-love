ce80a5708c168f93793fec840d8a92f7616515e5
Lando Icaza <landoicco@gmail.com>
2023-08-18 16:28:18 -0600

    Basic player movement added - player table defined

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 -- Set window properties
 local windowWidth, windowHeight = 800, 600
-local player = love.graphics.newImage("survivor.png")
+local playerSprite = love.graphics.newImage("survivor.png")
 local playerAngle = 0
+local playerSpeed = 1.7
 
 love.window.setTitle("Puto el k lea esto")
 love.window.setMode(windowWidth, windowHeight, {
@@ -11,24 +12,17 @@ love.window.setMode(windowWidth, windowHeight, {
 
 -- Define spawn position for the player
 function setPlayerSpawnPosition()
-    playerHeight = player:getHeight()
-    playerWidth = player:getWidth()
+    local playerHeight = playerSprite:getHeight()
+    local playerWidth = playerSprite:getWidth()
 
-    playerXPos = (windowWidth / 2) - (playerWidth / 2)
-    playerYPos = (windowHeight / 2) - (playerHeight / 2)
-end
+    local playerXPos = (windowWidth / 2) - (playerWidth / 2)
+    local playerYPos = (windowHeight / 2) - (playerHeight / 2)
 
--- Detect keyboard inputs every frame
-function love.keypressed(key)
-    if key == "e" then
-        text = "E key has been pressed!"
-    end
-end
-
-function love.keyreleased(key)
-    if key == "e" then
-        text = "E key has been released!"
-    end
+    player = {}
+    player.x = playerXPos
+    player.y = playerYPos
+    player.w = playerWidth
+    player.h = playerHeight
 end
 
 -- This funtion is called one at the beginning of the game
@@ -37,9 +31,22 @@ function love.load()
 end
 
 function love.update(dt)
+    -- Move the player
+    if love.keyboard.isDown("w") then
+        player.y = player.y - playerSpeed
+    end
+    if love.keyboard.isDown("s") then
+        player.y = player.y + playerSpeed
+    end
+    if love.keyboard.isDown("a") then
+        player.x = player.x - playerSpeed
+    end
+    if love.keyboard.isDown("d") then
+        player.x = player.x + playerSpeed
+    end
 end
 
 -- Callback function used to draw on screen every frame
 function love.draw()
-    love.graphics.draw(player, playerXPos, playerYPos)
+    love.graphics.draw(playerSprite, player.x, player.y, playerAngle)
 end
